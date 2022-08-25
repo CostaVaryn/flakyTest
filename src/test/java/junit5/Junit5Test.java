4a26731a3319c51483bfdbc3dc2bfec097d9c256
Costa Varyn <d3tug@yandex.ru>
2022-08-25 09:15:48 +0300

    add Junit5Test.java

---
+++ src/test/java/junit5/Junit5Test.java
@@ -0,0 +1,17 @@
+package junit5;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class Junit5Test {
+
+    @Test
+    public void testJunit5(){
+        Assertions.assertFalse(true);
+    }
+
+    @Test
+    public void testJunit52(){
+        Assertions.assertFalse(true);
+    }
+}
